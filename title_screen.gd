0208f9b04fc5956ce572988011cf12768c32794d
ymbrown <yayab789@gmail.com>
2024-03-17 09:05:03 -0400

    reworked title screen flash code

--- title_screen.gd
+++ title_screen.gd
@@ -1,34 +1,18 @@
 extends Control
 @onready var button = $StartGame
-var speed = 5
-var time = 0
-var sin_time = 0
-var pulsed = false
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	time += delta*speed
-	sin_time = sin(time*speed)
-
-	if sin_time > 0 and pulsed == false:
-		pulsed = true
-		button.modulate = Color(randf(), randf(), randf(), 1)
-	elif sin_time < 0 and pulsed == true:
-		pulsed = false
-
+@onready var timer = $Flash
 
 func _on_start_game_mouse_entered():
-	speed = 10
-	
+	timer.wait_time = 0.1
+
 
 func _on_start_game_mouse_exited():
-	speed = 5
+	timer.wait_time = 1
 
 
 func _on_start_game_pressed():
 	pass # Replace with function body.
 
+
+func _on_flash_timeout():
+	button.modulate = Color(randf(), randf(), randf(), 1)
