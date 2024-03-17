70a34ce151d936667569f43b3b5dea052c535b31
Tom T <37310147+Twads9816@users.noreply.github.com>
2024-03-17 12:43:58 -0400

    Starts customer functionality

--- main/main.gd
+++ main/main.gd
@@ -19,6 +19,8 @@ var correct_pattern = []
 
 var flag_id = null
 
+var customer_id: int = 0
+
 const PATTERNS = [
 	[1,2,3,0,0,0,3,2,1,0,0,0],
 	[2,1,3,0,0,2,0,1,0,3,0,0],
@@ -37,19 +39,38 @@ const PATTERNS = [
 	[2,1,3,0,0,0,3,1,2,0,0,0],
 	[3,2,1,0,0,0,1,2,3,0,0,0],
 	[0,0,0,3,1,2,2,0,0,1,3,0],
-]
+] 
 
 func _ready():
 	randomize_flag()
 
-func _unhandled_key_input(event):
-	$Customer/AnimationPlayer.play("main")
+func _unhandled_key_input(_event):
+	var tween = create_tween()
+	tween.tween_property($Line/Customer2, "position", Vector2(136,0), 0.5)
+	tween.parallel().tween_property($Line/Customer3, "position", Vector2(0,0), 0.5)
+	tween.parallel().tween_property($Line/Customer1, "position", Vector2(416,0), 0.75)
+	tween.tween_property($Line/Customer1, "position", Vector2(416,-64), 0.25)
+	tween.tween_callback($Bubble.set_visible.bind(true)).set_delay(0.5)
 
 func _on_button_pressed():
+	#Set trust
 	if sockets.map(func(socket): return socket.color_id) == correct_pattern:
 		$Trust.value += 10
 	else:
 		$Trust.value -= 10
+	#Teleport customer
+	$Line.get_child(customer_id).position = Vector2(-136,0)
+	$Bubble.visible = false
+	#Set new customer
+	customer_id = (customer_id + 1)%3
+	#Move line
+	var tween = create_tween()
+	tween.tween_property($Line.get_child((customer_id + 1)%3), "position", Vector2(136,0), 0.5)
+	tween.parallel().tween_property($Line.get_child((customer_id + 2)%3), "position", Vector2(0,0), 0.5)
+	tween.parallel().tween_property($Line.get_child(customer_id), "position", Vector2(416,0), 0.75)
+	tween.tween_property($Line.get_child(customer_id), "position", Vector2(416,-64), 0.25)
+	tween.tween_callback($Bubble.set_visible.bind(true)).set_delay(0.5)
+	
 	randomize_flag()
 
 func randomize_flag():
